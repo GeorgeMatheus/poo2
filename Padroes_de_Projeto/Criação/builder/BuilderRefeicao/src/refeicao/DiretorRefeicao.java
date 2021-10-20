dd1717233c7b8d003b53fe1c45476c2d52d2b2a5
TheeuzinN <matheuss308@gmail.com>
2021-10-20 17:14:05 -0300

    Tarefa - Builder - Refeição(Lanchonete)
    Atividade DOO

--- Padroes_de_Projeto/Criação/builder/BuilderRefeicao/src/refeicao/DiretorRefeicao.java
+++ Padroes_de_Projeto/Criação/builder/BuilderRefeicao/src/refeicao/DiretorRefeicao.java
@@ -9,21 +9,31 @@ public class DiretorRefeicao {
     
     public Refeicao preparaNaoVegetariana(String sabor, String tipoBebida){       
         Refeicao novaRefeicao = new Refeicao();
-        NonVegetarianoBuilder refeicaoBuilder = new NonVegetarianoBuilder(novaRefeicao);        
-        if(tipoBebida.equalsIgnoreCase("refrigerante"))
+        NonVegetarianoBuilder refeicaoBuilder = new NonVegetarianoBuilder(novaRefeicao);
+
+        if(tipoBebida.equalsIgnoreCase("refrigerante")){
             refeicaoBuilder.adicionarRefrigerante(sabor);
-        else
-            refeicaoBuilder.adicionarSuco(sabor);                
+
+        } else{
+            refeicaoBuilder.adicionarSuco(sabor);
+        }
+
+        refeicaoBuilder.preparaBatata();
         refeicaoBuilder.prepadaHamburgerCarne();
         return novaRefeicao;        
     }     
     public Refeicao preparaVegetariana(String sabor, String tipoBebida){        
         Refeicao novaRefeicao = new Refeicao();
-        VegetarianoBuilder refeicaoBuilder = new VegetarianoBuilder(novaRefeicao);        
-        if(tipoBebida.equalsIgnoreCase("refrigerante"))
+        VegetarianoBuilder refeicaoBuilder = new VegetarianoBuilder(novaRefeicao);
+
+        if(tipoBebida.equalsIgnoreCase("refrigerante")){
             refeicaoBuilder.adicionarRefrigerante(sabor);
-        else
-            refeicaoBuilder.adicionarSuco(sabor);     
+
+        } else{
+            refeicaoBuilder.adicionarSuco(sabor);
+        }
+
+        refeicaoBuilder.preparaBatata();
         refeicaoBuilder.preparaHamburgerVegetariano();
         return novaRefeicao;        
     }            
